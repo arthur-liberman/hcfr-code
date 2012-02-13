3c591432d8eb414b573564dbf7f6e777412e20ef
John Adcock <john@theadcocks.co.uk>
2012-02-13 22:51:50 +0000

    Create vs 2010 projects and fix compilation issues

--- StdAfx.h
+++ StdAfx.h
@@ -6,7 +6,13 @@
 #if !defined(AFX_STDAFX_H__53A7E878_541F_4044_A0E6_3A5BE7C30A41__INCLUDED_)
 #define AFX_STDAFX_H__53A7E878_541F_4044_A0E6_3A5BE7C30A41__INCLUDED_
 
+#if _MSC_VER >= 1600
+// visual studio 2010 ships with mfc that doesn't like 
+// low values of WINVER
+#define WINVER 0x0501
+#else
 #define WINVER 0x0410
+#endif
 #define _WIN32_IE 0x0601
 #define _CRT_SECURE_NO_WARNINGS
 // disable warning C4786: symbol greater than 255 character, okay to ignore
