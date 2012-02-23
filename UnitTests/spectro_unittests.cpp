164e92f32149337bb19edc2ece14d1b0a10b06b6
John Adcock <adcockj@users.sourceforge.net>
2012-02-23 17:12:07 +0000

    Provide a simple test of the numsup lib to confirm expected behavior of one of the endian related functions

--- UnitTests/spectro_unittests.cpp
+++ UnitTests/spectro_unittests.cpp
@@ -1,6 +1,6 @@
 #include <cppunit/config/SourcePrefix.h>
 #include <cppunit/extensions/HelperMacros.h>
-
+#include <numsup.h>
 
 #define THIS_TEST_CASE SpectroTestCase
 
@@ -18,9 +18,11 @@ public:
 protected:
     void floatConversion()
     {
-        CPPUNIT_ASSERT_DOUBLES_EQUAL( 1.0, 1.1, 0.05 );
-        CPPUNIT_ASSERT( 1 == 0 );
-        CPPUNIT_ASSERT( 1 == 1 );
+        // test that the conversion from int to double
+        // works correctly, was having issues with this function inside
+        // the spectro lib
+        double result = IEEE754todouble(1034279276);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL( 0.080981105566024780, result, 0.00000000000001 );
     }
 };
 
