53053a548af7ee892eb680a6e1916733bc52a239
John Adcock <adcockj@users.sourceforge.net>
2012-02-28 17:19:43 +0000

    Update and sign all driver files

--- Tools/libusb1/Hcfr_windows_build.cmd
+++ Tools/libusb1/Hcfr_windows_build.cmd
@@ -2,7 +2,7 @@
 rem **************************************************************************
 rem build the x86 version of the driver and dll
 rem **************************************************************************
-if "%DDK_VER%" == "" set DDK_VER=7600.16385.1
+if "%DDK_VER%" == "" set DDK_VER=7600.16385.0
 setlocal
 call c:\WinDDK\%DDK_VER%\bin\setenv.bat c:\WinDDK\%DDK_VER%\ fre x86 WXP no_oacr
 cd /d %~dp0%
