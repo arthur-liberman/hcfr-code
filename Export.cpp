1b4df3ef6df2cfaccde8f5bb1dbdc42c27d73ad6
zoyd000 <zoyd000@gmail.com>
2015-05-31 15:50:58 +0000

    Add libpdf

--- Export.cpp
+++ Export.cpp
@@ -30,12 +30,63 @@
 #include "ExportReplaceDialog.h"
 
 #include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <setjmp.h>
+#include "libharu\include\hpdf.h"
 
-#ifdef _DEBUG
-#undef THIS_FILE
-static char THIS_FILE[]=__FILE__;
-#define new DEBUG_NEW
+jmp_buf env;
+
+void error_handler  (HPDF_STATUS   error_no,
+                HPDF_STATUS   detail_no,
+                void         *user_data)
+{
+	char msg[50];
+//    printf ("ERROR: error_no=%04X, detail_no=%u\n", (HPDF_UINT)error_no,
+//                (HPDF_UINT)detail_no);
+    sprintf (msg,"ERROR: error_no=%04X, detail_no=%u\n", (HPDF_UINT)error_no,
+                (HPDF_UINT)detail_no);
+			if(GetColorApp()->InMeasureMessageBox(msg,"PDF ERROR",MB_YESNO)!=IDYES)
+    longjmp(env, 1);
+}
+
+void
+draw_image (HPDF_Doc     pdf,
+            const char  *filename,
+            float        x,
+            float        y,
+            const char  *text)
+{
+#ifdef __WIN32__
+    const char* FILE_SEPARATOR = "\\";
+#else
+    const char* FILE_SEPARATOR = "/";
 #endif
+    char filename1[255];
+
+    HPDF_Page page = HPDF_GetCurrentPage (pdf);
+    HPDF_Image image;
+
+    strcpy(filename1, "pngsuite");
+    strcat(filename1, FILE_SEPARATOR);
+    strcat(filename1, filename);
+
+    image = HPDF_LoadPngImageFromFile (pdf, filename1);
+
+    /* Draw image to the canvas. */
+    HPDF_Page_DrawImage (page, image, x, y, HPDF_Image_GetWidth (image),
+                    HPDF_Image_GetHeight (image));
+
+    /* Print the text. */
+    HPDF_Page_BeginText (page);
+    HPDF_Page_SetTextLeading (page, 16);
+    HPDF_Page_MoveTextPos (page, x, y);
+    HPDF_Page_ShowTextNextLine (page, filename);
+    HPDF_Page_ShowTextNextLine (page, text);
+    HPDF_Page_EndText (page);
+}
+
 
 // check the Excel-ODBC driver is installed
 bool IsExcelDriverInsalled()
@@ -165,6 +216,95 @@ bool CExport::Save()
 				return false;
 		}
 	}
+
+	if(true){
+    HPDF_Doc  pdf;
+    HPDF_Font font;
+    HPDF_Page page;
+    char fname[256];
+    HPDF_Destination dst;
+
+    strcpy (fname, "test");
+    strcat (fname, ".pdf");
+
+    pdf = HPDF_New (error_handler, NULL);
+
+    if (!pdf) {
+		CString	Msg, Title;
+		Msg.LoadString ( IDS_ERREXPORT );
+		Title.LoadString ( IDS_EXPORT );
+		GetColorApp()->InMeasureMessageBox(Msg+fname+"error: cannot create PdfDoc object\n"+m_errorStr,Title,MB_OK);
+        return 1;
+    }
+
+    /* error-handler */
+    if (setjmp(env)) {
+        HPDF_Free (pdf);
+        return 1;
+    }
+
+    HPDF_SetCompressionMode (pdf, HPDF_COMP_ALL);
+
+    /* create default-font */
+    font = HPDF_GetFont (pdf, "Helvetica", NULL);
+
+    /* add a new page object. */
+    page = HPDF_AddPage (pdf);
+
+    HPDF_Page_SetWidth (page, 550);
+    HPDF_Page_SetHeight (page, 650);
+
+    dst = HPDF_Page_CreateDestination (page);
+    HPDF_Destination_SetXYZ (dst, 0, HPDF_Page_GetHeight (page), 1);
+    HPDF_SetOpenAction(pdf, dst);
+
+    HPDF_Page_BeginText (page);
+    HPDF_Page_SetFontAndSize (page, font, 20);
+    HPDF_Page_MoveTextPos (page, 220, HPDF_Page_GetHeight (page) - 70);
+    HPDF_Page_ShowText (page, "PngDemo");
+    HPDF_Page_EndText (page);
+
+    HPDF_Page_SetFontAndSize (page, font, 12);
+
+    draw_image (pdf, "basn0g01.png", 100, HPDF_Page_GetHeight (page) - 150,
+                "1bit grayscale.");
+    draw_image (pdf, "basn0g02.png", 200, HPDF_Page_GetHeight (page) - 150,
+                "2bit grayscale.");
+    draw_image (pdf, "basn0g04.png", 300, HPDF_Page_GetHeight (page) - 150,
+                "4bit grayscale.");
+    draw_image (pdf, "basn0g08.png", 400, HPDF_Page_GetHeight (page) - 150,
+                "8bit grayscale.");
+
+    draw_image (pdf, "basn2c08.png", 100, HPDF_Page_GetHeight (page) - 250,
+                "8bit color.");
+    draw_image (pdf, "basn2c16.png", 200, HPDF_Page_GetHeight (page) - 250,
+                "16bit color.");
+
+    draw_image (pdf, "basn3p01.png", 100, HPDF_Page_GetHeight (page) - 350,
+                "1bit pallet.");
+    draw_image (pdf, "basn3p02.png", 200, HPDF_Page_GetHeight (page) - 350,
+                "2bit pallet.");
+    draw_image (pdf, "basn3p04.png", 300, HPDF_Page_GetHeight (page) - 350,
+                "4bit pallet.");
+    draw_image (pdf, "basn3p08.png", 400, HPDF_Page_GetHeight (page) - 350,
+                "8bit pallet.");
+
+    draw_image (pdf, "basn4a08.png", 100, HPDF_Page_GetHeight (page) - 450,
+                "8bit alpha.");
+    draw_image (pdf, "basn4a16.png", 200, HPDF_Page_GetHeight (page) - 450,
+                "16bit alpha.");
+
+    draw_image (pdf, "basn6a08.png", 100, HPDF_Page_GetHeight (page) - 550,
+                "8bit alpha.");
+    draw_image (pdf, "basn6a16.png", 200, HPDF_Page_GetHeight (page) - 550,
+                "16bit alpha.");
+
+    /* save the document to a file */
+    HPDF_SaveToFile (pdf, fname);
+
+    /* clean up */
+    HPDF_Free (pdf);
+	}	
 
 	return SaveSheets();
 }
